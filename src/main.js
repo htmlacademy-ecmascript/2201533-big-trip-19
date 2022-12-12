c8069e1835da67b253b480fcd8ece87b7b7de718
OEM Configuration (temporary user) <89179699365@mail.ru>
2022-12-12 14:20:31 +0400

    20221212 1420

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 console.log('main.js');
+
+DATA_URL = 'https://19.ecmascript.pages.academy/big-trip/destinations';
+
+const loadData = (renderAds)=>
+  fetch(DATA_URL,{
+  headers: {
+    Authorization: 'Basic er8hg83jdr543w',
+  }
+  })
+    .then((response)=>{
+      if (response.ok){
+        return response.text();
+      }
+      throw `status: ${response.status},
+        statusText: ${response.statusText}`;
+    })
+    .then(console.log)
+    .catch(console.log);
+
+
+loadData()
+  .then(r => console.log(r))
